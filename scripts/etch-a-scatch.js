133f018420120378ac83a75516072f47ab0531a4
bhp <vovafc87@gmail.com>
2023-12-24 13:43:57 +0200

    added dynamicly changable desk size

--- scripts/etch-a-scatch.js
+++ scripts/etch-a-scatch.js
@@ -1,10 +1,16 @@
 const squaresContainer = document.querySelector('.squares-container');
 
 const createGrid = size => {
+    const MAX_WIDTH = 958;
+
+
+
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
             const squareDiv = document.createElement('div');
             squareDiv.classList.add('square');
+            squareDiv.style.width = `${(MAX_WIDTH / size) }px`;
+            squareDiv.style.height = `${(MAX_WIDTH / size)}px`;
             squaresContainer.appendChild(squareDiv);
         }
     }
@@ -21,10 +27,13 @@ const handleMouseOver = event => {
 }
 
 const resetGrid = () => {
-    const size = 16;
-    squaresContainer.innerHTML = '';
+    const newSize = prompt("Enter a new grid size");
+    if (newSize === null || newSize === '') return;
+    if (newSize < 1 || newSize > 100) return alert("Please enter a number between 1 and 100");
 
-    createGrid(size);
+
+    squaresContainer.innerHTML = '';
+    createGrid(newSize);
 }
 
 // Initial grid creation
